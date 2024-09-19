7797ddedb0f792a2b0a816bbcb6fabb186c1f0a1
aragami3070 <smirnov17612@gmail.com>
2024-09-20 01:05:23 +0400

    Added and use public checkDate and private checkMonthDay

--- homework1/libs/Date/Date.h
+++ homework1/libs/Date/Date.h
@@ -2,22 +2,31 @@
 
 #include <string>
 
-//класс для даты
+// класс для даты
 class Date{
 private:
     int Day;
     int Month;
     int Year;
+
+    bool checkMonthDay(int day, int month, int year);
 public:
+    // задать дату равную 0.0.0 можно только через setDate или инициализацию
     Date(int Day = 0, int Month = 0, int Year = 0);
     ~Date();
 
-    std::string getData();
+    // гетторы
+    std::string getDate();
     int getDay();
     int getMonth();
     int getYear();
 
+    // проверка на валидность задаваемых дат
+    bool checkDate();
+
+    // сетторы
     void setDay(int newDay);
     void setMonth(int newMonth);
     void setYear(int newYear);
+    void setDate(Date newDate);
 };
