6c8ea34a88d2e9d3759e44f3403bb320904fbea1
aragami3070 <smirnov17612@gmail.com>
2024-10-21 09:02:19 +0400

    Add: Plane class description

--- homework2/libs/Transports/Plane/Plane.h
+++ homework2/libs/Transports/Plane/Plane.h
@@ -1,5 +1,55 @@
 #include "../Transport.h"
 
 class Plane:Transport{
+private:
+    int WingsNumber;    // Кол-во крыльев
+    bool IsScrewPlane;  // Это винтовой самолет или нет
+public:
+    // Конструктор
+    Plane(string model, double enginePower, double maxSpeed,
+        double weight, double cost, int maxNumOfUses, 
+        int wingsNumber, bool isScrewPlane):
+        Transport(model, enginePower, maxSpeed, weight, cost, maxNumOfUses){
+        WingsNumber = wingsNumber;
+        IsScrewPlane = isScrewPlane;
+    };
+    // Конструктор копирования
+    Plane(Plane &plane):Transport(plane.Model, plane.EnginePower, plane.MaxSpeed,
+                            plane.Weight, plane.Cost, plane.getMaxNumOfUses()){
+        WingsNumber = plane.WingsNumber;
+        IsScrewPlane = plane.IsScrewPlane;
+    }
 
+    Plane(){ setStatusErr(); }
+
+    // Деструктор
+    ~Plane();
+
+    // get методы
+    int getWingsNumber();
+    bool getIsScrewPlane();
+
+    // set методы
+    void setWingsNumber(string newBrand);
+    void setIsScrewPlane(int newDorsNumber);
+
+    // Plane методы
+    bool tryCheckScrew();
+
+    // перезаписанные методы Transport
+
+    // Вывести все
+    void printall();
+
+    // Сравнить два транспорта
+    bool compareTransport(Plane* otherPlane);
+
+    // Может ли транспорт поехать/полететь (быть использованым)
+    bool tryGo();
+
+    // Можно ли перевезти данное кол-во груза
+    bool tryTransferCargo(double weightCargo);
+
+    // Можно ли перевезти данное кол-во людей
+    bool tryTransferPeople(int numberOfPassenger);
 };
