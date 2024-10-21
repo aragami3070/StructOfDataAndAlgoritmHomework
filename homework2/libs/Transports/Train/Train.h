bd8285d3917b3e3ad690f474bf10dd43478b085a
aragami3070 <smirnov17612@gmail.com>
2024-10-22 00:19:58 +0400

    Add: description Train class

--- homework2/libs/Transports/Train/Train.h
+++ homework2/libs/Transports/Train/Train.h
@@ -1,5 +1,57 @@
 #include "../Transport.h"
 
 class Train:Transport{
+private:
+    int WagonsNumber;   // Кол-во вагонов
+    bool IsSteamTrain;  // Это паровоз
+public:
+    // Конструктор
+    Train(string model, double enginePower, double maxSpeed,
+        double weight, double cost, int maxNumOfUses, 
+        int wagonsNumber, bool isSteamTrain):
+        Transport(model, enginePower, maxSpeed, weight, cost, maxNumOfUses){
+        WagonsNumber = wagonsNumber;
+        IsSteamTrain = isSteamTrain;
+    };
 
+    // Конструктор копирования
+    Train(Train &train):Transport(train.Model, train.EnginePower, train.MaxSpeed,
+                            train.Weight, train.Cost, train.getMaxNumOfUses()){
+        WagonsNumber = train.WagonsNumber;
+        IsSteamTrain = train.IsSteamTrain;
+    }
+
+    Train(){ setStatusErr(); }
+
+    // Деструктор
+    ~Train();
+
+    // get методы
+
+    int getWagonsNumber();
+    bool getIsSteamTrain();
+
+    // set методы
+
+    void setWagonsNumber(int newWagonsNumber);
+    void setIsSteamTrain(bool newIsSteamTrain);
+
+    // Train методы
+
+    // перезаписанные методы Transport
+
+    // Вывести все
+    void printall();
+
+    // Сравнить два транспорта
+    bool compareTransport(Train* otherTrain);
+
+    // Может ли транспорт поехать/полететь (быть использованым)
+    bool tryGo();
+
+    // Можно ли перевезти данное кол-во груза
+    bool tryTransferCargo(double weightCargo);
+
+    // Можно ли перевезти данное кол-во людей
+    bool tryTransferPeople(int numberOfPassenger);
 };
