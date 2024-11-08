644ab6198f7ba4535b62baecec50232740a2c5fb
aragami3070 <smirnov17612@gmail.com>
2024-11-08 15:16:58 +0400

    Add: Errors class

---
+++ homework3/libs/Errors/Errors.h
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+class SomeError
+{
+public:
+	virtual void print() { cerr << "Some error" << endl; }
+};
+
+class NotZero : public SomeError {
+public:
+	NotZero(){}
+	void print() { cerr << "Error[418]: Price and weight can't be <=0" << endl; }
+
+};
+
+class DateBroken : public SomeError
+{
+public:
+	DateBroken(){}
+	void print() { cerr << "Error[418]: Date of manufacture can't be 0.0.0"  << endl; }
+};
