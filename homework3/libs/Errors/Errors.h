4a727527c101c2eb9f72ceafeb9ea86605e0ce0a
aragami3070 <smirnov17612@gmail.com>
2024-11-20 22:17:27 +0400

    Add: new class Errors DeleteFromEmptyList

--- homework3/libs/Errors/Errors.h
+++ homework3/libs/Errors/Errors.h
@@ -16,9 +16,14 @@ public:
 
 };
 
-class DateBroken : public SomeError
-{
+class DateBroken : public SomeError{
 public:
 	DateBroken(){}
 	void print() { cerr << "Error[418]: Date of manufacture can't be 0.0.0"  << endl; }
+};
+
+class DeleteFromEmptyList : public SomeError {
+public:
+	DeleteFromEmptyList() {}
+	void print() { cerr << "Error: This List empty and this elem can't be deleted" << endl; }
 };
